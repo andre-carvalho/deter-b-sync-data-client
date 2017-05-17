9a861db49fa894b6eb4e7852906c4564bc930edb
andre.carvalho <andre.carvalho@funcate.org.br>
2017-05-17 17:09:16 -0300

    Install script to prepare environment was created.

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+error_reporting(E_ALL & ~E_NOTICE);
+
 use Services\PostgreSQLDataService;
 use Services\PostgreSQLLogService;
 use Services\HTTPSyncService;
@@ -19,6 +21,23 @@ use DAO\GeneralLog;
 $log = new GeneralLog();// to write more detailed log in file.
 $pgDataService = new PostgreSQLDataService();
 $pgLogService = new PostgreSQLLogService();
+
+if(get_class($log)!=='DAO\\GeneralLog') {
+	// disable logs in file
+	$log=false;
+}
+
+if(get_class($pgDataService)!=='Services\\PostgreSQLDataService') {
+	// Abort the process because we don't access the database server to write data.
+	if($log) $log->writeErrorLog("Abort the process because we don't access the database server to write data.");
+	exit();
+}
+
+if(get_class($pgLogService)!=='Services\\PostgreSQLLogService') {
+	// Abort the process because we don't access the database server to write log.
+	if($log) $log->writeErrorLog("Abort the process because we don't access the database server to write log.");
+	exit();
+}
 
 $error = "";// Used to get the error description from PostgreSQLService methods calls.
 
@@ -29,9 +48,9 @@ $RAWFILE = $syncService->downloadAllGeometries();
 
 if($RAWFILE===false) {
 	$error = "Failure on download data.";
-	$log->writeErrorLog($error);
+	if($log) $log->writeErrorLog($error);
 	if(!$pgLogService->writeLog(0, $error, $RAWFILE)) {
-		$log->writeErrorLog("Failure on write the error on log table.");
+		if($log) $log->writeErrorLog("Failure on write the error on log table.");
 	}
 	exit();
 }
@@ -42,14 +61,14 @@ $data = file_get_contents( $RAWFILE );
 if($data===false) {
 	$error = "Fail to load all data from file to memory.";
 	if(!$pgLogService->writeLog(0, $error, $RAWFILE)) {
-		$log->writeErrorLog("Failure on write the error on log table.");
+		if($log) $log->writeErrorLog($error . "\nFailure on write the error on log table.");
 	}
 	exit();
 }
 
 if(!$pgDataService->renewDataTable($data, $error)) {
 	if(!$pgLogService->writeLog(0, $error, $RAWFILE)) {
-		$log->writeErrorLog("Failure on write the error on log table.");
+		if($log) $log->writeErrorLog($error . "\nFailure on write the error on log table.");
 	}
 	exit();
 }
